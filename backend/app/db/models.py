e3abd24a0259cfc913bfd8cef5fb972cff91392e
Ori Katz <orikatz99@walla.co.il>
2026-03-12 19:09:24 +0200

    add Board model and relationship with User

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,10 +1,20 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
+#user model
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     guest_name = Column(String, nullable=False)
+    boards = relationship("Board", back_populates="user")
+
+#board model
+class Board(Base):
+    __tablename__ = "boards"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="boards")
